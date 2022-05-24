cbbc3c8981e9efe5fd6259a0651e45ea860a095d
Abhay Ekanath <abhayekanath@gmail.com>
2022-05-24 23:42:00 +0530

    First

---
+++ index.php
@@ -0,0 +1,312 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <title>Abhay Ekanath</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="ThemeZaa">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta name="description" content="Litho is a clean and modern design, BootStrap 5 responsive, business and portfolio multipurpose HTML5 template with 37+ ready homepage demos.">
+    <!-- favicon icon -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    <!-- style sheets and font icons  -->
+    <link rel="stylesheet" type="text/css" href="css/font-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="css/theme-vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+  </head>
+  <body>
+    <!-- start header -->
+    <header>
+      <!-- start navigation -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-transparent header-dark fixed-top navbar-boxed navbar-static-overlay">
+        <div class="container-fluid h-100px md-h-70px">
+          <div class="col-6 col-lg-2 me-auto me-auto ps-lg-0">
+            <a class="navbar-brand" href="index.html">
+            <img src="images/symbol-black.png" data-at2x="images/symbol-black@2x.png" class="default-logo" alt="">
+            <img src="images/symbol-black.png" data-at2x="images/symbol-black@2x.png" class="alt-logo" alt="">
+            <img src="images/symbol-black.png" data-at2x="images/symbol-black@2x.png" class="mobile-logo" alt="">
+            </a>
+          </div>
+          <div class="col-auto text-end">
+            <div class="header-push-button">
+              <a href="javascript:void(0);" class="push-button ">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- end navigation -->
+      <!-- start hamburger menu popup -->
+      <div class="hamburger-menu hamburger-menu-big-font bg-black w-25 xl-w-60 lg-w-70 md-w-50 sm-w-100">
+        <a href="javascript:void(0);" class="close-menu text-white"><i class="ti-close"></i></a>
+        <div class="row g-0 h-100">
+          <div class="col-lg-12">
+            <div class="row g-0 align-items-center justify-content-center h-100 padding-7-rem-all xs-padding-3-rem-all">
+              <div class="col-12 menu-list-wrapper menu-list-wrapper-small text-center text-md-start" data-scroll-options='{ "theme": "light" }'>
+                <!-- start menu -->
+                <ul class="menu-list alt-font w-100">
+                  <!-- start menu item -->
+                  <li class="menu-list-item"><a href="index.html">home</a>
+                  </li>
+                  <!-- end menu item -->
+                  <!-- start menu item -->
+                  <li class="menu-list-item"><a href="about-me.html">about</a></li>
+                  <!-- end menu item -->
+                  <!-- start menu item -->
+                  <li class="menu-list-item"><a href="our-services.html">services</a></li>
+                  <!-- end menu item -->
+                  <!-- start menu item -->
+                  <li class="menu-list-item"><a href="portfolio-colorful-metro.html">portfolio</a></li>
+                  <!-- end menu item -->
+                  <!-- start menu item -->
+                  <li class="menu-list-item"><a href="home-magazine.html">blog</a></li>
+                  <!-- end menu item -->
+                  <!-- start menu item -->
+                  <li class="menu-list-item"><a href="contact-us-modern.html">contact</a></li>
+                  <!-- end menu item -->
+                </ul>
+                <!-- end menu -->
+              </div>
+              <div class="col-12 d-none d-md-block">
+                <div class="alt-font margin-50px-top">
+                  <span class="margin-10px-bottom font-weight-300 text-extra-large d-inline-block">Let's work together?</span>
+                  <h6><a href="mailto:info@domain.com" class="text-yellow text-decoration-line-bottom text-white-hover">info@domain.com</a></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end hamburger menu popup -->
+    </header>
+    <!-- end header -->
+    <!-- start hero section -->
+    <section id="home" class="p-0 parallax mobile-height" data-parallax-background-ratio="0.5" style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/5/5d/Low-key_photography_-_portrait_of_a_young_man.jpg');">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-xl-4 col-md-5 position-relative full-screen d-flex flex-column justify-content-center md-landscape-h-600px">
+            <h2 class="alt-font font-weight-300 letter-spacing-minus-2px text-extra-dark-gray text-uppercase margin-15px-bottom">What's up,  i am</h2>
+            <div class="alt-font cd-headline push">
+              <h1 class="cd-words-wrapper text-extra-dark-gray text-uppercase title-large letter-spacing-minus-5px sm-letter-spacing-minus-1-half">                                
+                <b class="is-visible font-weight-700">Abhay<br/>Ekanath</b>
+                <b class="font-weight-700">UI<br/>Designer</b>
+              </h1>
+            </div>
+            <div class="position-absolute left-15px bottom-100px sm-bottom-50px social-icon-style-12">
+              <ul class="medium-icon">
+                <li class="m-0"><a class="facebook text-start" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                <li class="m-0"><a class="instagram text-start" href="http://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                <li class="m-0"><a class="twitter text-start" href="http://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                <li class="m-0"><a class="dribbble text-start" href="http://www.dribbble.com" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end hero section -->
+    <!-- start section -->
+    <section class="big-section">
+      <div class="container">
+        <div class="row align-content-center">
+          <div class="col-12 col-lg-10 md-margin-30px-bottom text-center text-sm-start wow animate__fadeIn" data-wow-delay="0.2s">
+            <h6 class="alt-font text-extra-dark-gray line-height-40px w-90 mb-0 lg-w-100 sm-line-height-26px">A creative user interface designer with a love for minimal design, clean layout and well-written code, located in <span class="font-weight-700 text-extra-dark-gray text-decoration-line-bottom-medium">Kochi</span> i enjoy creative problem solving, strategizing concepts, developing prototypes and pushing boundaries of creativity. And interested in User research, Interaction design and front-end web development.</h6>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end section -->
+    <!-- start section -->
+    <section class="py-0">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 filter-content px-0">
+            <ul class="portfolio-overlay portfolio-wrapper grid grid-loading grid-4col xl-grid-4col lg-grid-3col md-grid-2col sm-grid-2col xs-grid-1col text-center">
+              <li class="grid-sizer"></li>
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn">
+                <a href="single-project-page-01.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Daimler</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Brochure</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn" data-wow-delay="0.2s">
+                <a href="single-project-page-02.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Waterlly</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Branding</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn" data-wow-delay="0.4s">
+                <a href="single-project-page-03.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Leather</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Photography</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn" data-wow-delay="0.6s">
+                <a href="single-project-page-04.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Gardner</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Photography</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn">
+                <a href="single-project-page-05.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Manchester</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Photography</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn" data-wow-delay="0.2s">
+                <a href="single-project-page-01.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Armchair</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Photography</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn" data-wow-delay="0.4s">
+                <a href="single-project-page-02.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Truenorth</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Identity</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+              <!-- start portfolio item -->
+              <li class="grid-item wow animate__fadeIn" data-wow-delay="0.6s">
+                <a href="single-project-page-04.html">
+                  <div class="portfolio-box">
+                    <div class="portfolio-image bg-black">
+                      <img src="https://via.placeholder.com/800x800" alt="" />
+                      <div class="portfolio-hover justify-content-end d-flex flex-column padding-50px-tb xl-padding-30px-tb">
+                        <i class="ti-plus portfolio-plus-icon font-weight-300 text-yellow absolute-middle-center icon-extra-small move-top-bottom"></i>
+                        <h6 class="alt-font text-white font-weight-500 d-block text-uppercase mb-0">Cortifiel</h6>
+                        <div class="text-medium d-block text-white opacity-5 text-uppercase">Photography</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <!-- end portfolio item -->
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-yellow half-section home-personal-portfolio md-no-padding-bottom">
+      <div class="container-fluid">
+        <div class="row row-cols-1 align-items-center">
+          <div class="col col-md border border-right-0 border-left-0 border-bottom-0 border-dotted border-color-extra-dark-gray sm-margin-30px-bottom"></div>
+          <div class="col col-md-auto text-center padding-4-half-rem-lr md-padding-2-half-rem-lr xs-padding-1-half-rem-top xs-padding-2-rem-bottom wow animate__fadeIn" data-wow-delay="0.4s">
+            <span class="alt-font font-weight-300 title-large-2 text-extra-dark-gray letter-spacing-minus-3px d-block md-line-height-normal">Let's work <span class="font-weight-600">together?</span></span>
+          </div>
+          <div class="col col-md border border-right-0 border-left-0 border-bottom-0 border-dotted border-color-extra-dark-gray sm-margin-30px-top"></div>
+        </div>
+      </div>
+    </section>
+    <!-- end section -->
+    <!-- start footer -->
+    <footer class="footer-light bg-yellow padding-5-rem-tb">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12 col-lg-4 text-center text-lg-start md-margin-20px-bottom">
+            <span class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Call us today! +1 234 567 8910</span>
+          </div>
+          <div class="col-12 col-lg-4 text-center md-margin-20px-bottom">
+            <div class="social-icon-style-12">
+              <ul class="medium-icon">
+                <li><a class="facebook" href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a class="dribbble" href="http://www.dribbble.com" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+                <li><a class="twitter" href="http://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                <li><a class="instagram" href="http://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4 text-center text-lg-end last-paragraph-no-margin">
+            <p class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">&copy; 2021 Litho is Powered by <a href="https://www.themezaa.com/" target="_blank" class="text-decoration-line-bottom text-extra-dark-gray text-black-hover">ThemeZaa</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- end footer -->
+    <!-- start scroll to top -->
+    <a class="scroll-top-arrow" href="javascript:void(0);"><i class="feather icon-feather-arrow-up"></i></a>
+    <!-- end scroll to top -->
+    <!-- javascript -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/theme-vendors.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+  </body>
+</html>
